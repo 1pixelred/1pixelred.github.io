51130dd9056ad9c70631b4651757c8836b44beb2
Alex Baker <alex.baker.fon@gmail.com>
2023-02-28 18:38:46 +0200

    add: icon and title for wallet

--- src/flow/config.js
+++ src/flow/config.js
@@ -3,5 +3,7 @@ import { config } from "@onflow/fcl";
 config({
   "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "app.detail.icon": "https://1pixel.red/android-icon-192x192.png",
+  "app.detail.title": "1PIXEL.🟥",
   "0xPixel": "0x60fb6829e09510da",
 });
