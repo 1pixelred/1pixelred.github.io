4c369136f2eb437904ad636689e1e0076df7acbe
Alex Baker <alex.baker.fon@gmail.com>
2023-03-18 16:42:31 +0200

    del: reportWebVitals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { MantineProvider } from "@mantine/core";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 
@@ -42,8 +41,3 @@ root.render(
     </MantineProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
